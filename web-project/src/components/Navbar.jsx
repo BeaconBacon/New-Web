fcaccbf6da624326de65061ffea79873f5d57c8c
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-05-30 18:14:49 -0400

    navbar with mobile app scaling

--- web-project/src/components/Navbar.jsx
+++ web-project/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           <li
             key={nav.id}
             className={`font-poppins
-            font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}
+            font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white mr-10`}
           
           >
             <a href={`#${nav.id}`}>
@@ -23,13 +23,34 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      <div className = "sm:hidden flex flex-1 justify-end items-center">
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
         <img 
           src= {toggle ? close : menu}
           alt="menu"
           className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
         
         />
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px]
+        rounded-xl sidebar`}>
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins
+                font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mb-4"} text-white mr-10`}
+              
+              >
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+
+              </li>
+            ))}
+          </ul>
+          
+        </div>
       </div>
     </nav>
   )
