19925d283443105b82f32ad169ad1f553551ab07
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-06-07 16:44:15 -0500

    Get Started button fixed with correct padding

--- web-project/src/components/Business.jsx
+++ web-project/src/components/Business.jsx
@@ -8,10 +8,10 @@ const FeatureCard = ({ icon, title, content, index }) => (
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain"/>
     </div>
     <div className="flex-1 flex flex-col ml-3">
-      <h4>
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
         {title}
       </h4>
-      <p>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
         {content}
       </p>
     </div>
@@ -24,7 +24,7 @@ const Business = () => {
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /> we’ll handle the money.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
-      <Button styles="mt-10" />
+      <Button styles={`mt-10`} />
     </div>
     <div className={`${layout.sectionImg} flex-col`}>
       {features.map((feature, index) => (
